c848485450c539e17746c3572e07a8f1855f64a9
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-05-07 07:01:35 -0700

    Encuentra proyectos cuando el mismo existe e n una lista de un proyecto

--- src/buscador.spec.js
+++ src/buscador.spec.js
@@ -3,8 +3,17 @@ describe("Buscar", () => {
     let proyectos = [];
     expect(buscarProyecto("ejercicio1", proyectos)).toEqual("");
   });
+
+  it("encuentra proyectos cuando el mismo existe en una lista de un proyecto", () => {
+    let proyectos = [];
+    proyectos.push("miUnicoProyecto");
+    expect(buscarProyecto("miUnicoProyecto", proyectos)).toEqual("miUnicoProyecto");
+  });
 });
 
 function buscarProyecto(nombre, proyectos){
+  if(proyectos[0]===nombre){
+    return proyectos[0];
+  }
   return "";
 }
